b164602b79f978647f0db8b15cd7509994b67462
Bacon <baconator@users.noreply.github.com>
2016-03-20 18:47:53 -0700

    add namespaces

--- src/cmd-insert/src/app.cpp
+++ src/cmd-insert/src/app.cpp
@@ -4,15 +4,18 @@
 #include <iostream>
 #include <vector>
 
-static llvm::cl::opt<bool> Quiet("quiet",
-                                 llvm::cl::desc("Disable all output not relating to compilation."));
-static llvm::cl::list<std::string> Targets(llvm::cl::Positional,
-                                          llvm::cl::desc("[<file>,...]"),
-                                          llvm::cl::OneOrMore);
+namespace autopledge {
 
+    static llvm::cl::opt<bool> Quiet("quiet",
+                                     llvm::cl::desc("Disable all output not relating to compilation."));
+    static llvm::cl::list<std::string> Targets(llvm::cl::Positional,
+                                               llvm::cl::desc("[<file>,...]"),
+                                               llvm::cl::OneOrMore);
+
+}
 
 int main(int argc, char **argv) {
     llvm::cl::ParseCommandLineOptions(argc, argv);
 
-    
+
 }
