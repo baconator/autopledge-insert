336dd5e0876b84b488d6e9d0740a0acea6a8d287
Bacon <baconator@users.noreply.github.com>
2016-03-20 18:45:09 -0700

    switch to a cl::list of parameters

--- src/cmd-insert/src/app.cpp
+++ src/cmd-insert/src/app.cpp
@@ -1,24 +1,18 @@
 #include "llvm/Support/CommandLine.h"
 #include <string>
 #include <sstream>
+#include <iostream>
 #include <vector>
 
 static llvm::cl::opt<bool> Quiet("quiet",
                                  llvm::cl::desc("Disable all output not relating to compilation."));
-static llvm::cl::opt<std::string> Targets(llvm::cl::Positional,
+static llvm::cl::list<std::string> Targets(llvm::cl::Positional,
                                           llvm::cl::desc("[<file>,...]"),
-                                          llvm::cl::Required);
+                                          llvm::cl::OneOrMore);
 
 
 int main(int argc, char **argv) {
     llvm::cl::ParseCommandLineOptions(argc, argv);
 
-    // Psh, who manipulates _strings_? Better leave that out of the stdlib.
-    // 2d graphics you say? Hmm, good idea.
-    std::vector<std::string> targetNames;
-    std::stringstream ss(Targets.getValue());
-    std::string item;
-    while (std::getline(ss, item, ' ')) {
-        targetNames.push_back(item);
-    }
+    
 }
