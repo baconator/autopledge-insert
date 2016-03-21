e5da1312d63de58e54a953f82cc20a52b2cf31b6
Bacon <baconator@users.noreply.github.com>
2016-03-21 08:36:33 -0700

    added modern json dependency

--- src/libmodifysource/include/pass.hpp
+++ src/libmodifysource/include/pass.hpp
@@ -3,13 +3,13 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
+#include "json.hpp"
 #include <map>
 #include "syscall.hpp"
 
 namespace autopledge {
     struct AnnotateSyscalls : public llvm::ModulePass {
         static char ID;
-
         AnnotateSyscalls() : ModulePass(ID) { }
 
         bool runOnModule(llvm::Module& m) override { }
