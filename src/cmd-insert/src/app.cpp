940a88f9f464e4295fd98491f9180c0468ce4e83
Bacon <baconator@users.noreply.github.com>
2016-03-21 17:10:22 -0700

    included tooling in cmd-insert

--- src/cmd-insert/src/app.cpp
+++ src/cmd-insert/src/app.cpp
@@ -1,14 +1,13 @@
-#include "llvm/Support/CommandLine.h"
+#include <llvm/Support/CommandLine.h>
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <clang/Tooling/Tooling.h>
 
 namespace autopledge {
-
-    static llvm::cl::opt<bool> Quiet("quiet",
-                                     llvm::cl::desc("Disable all output not relating to compilation."));
-    static llvm::cl::list<std::string> Targets(llvm::cl::Positional,
+    // TODO: handle arbitrary input for handoff to compiler.
+    static llvm::cl::list<std::string> Files(llvm::cl::Positional,
                                                llvm::cl::desc("[<file>,...]"),
                                                llvm::cl::OneOrMore);
 
