fd6dc7ab54f73d598d9d09d1b5b59269505c9dbb
ygist <yla276@sfu.ca>
2016-03-26 12:14:03 -0700

    main function to parse IR

--- src/cmd-annotate/src/app.cpp
+++ src/cmd-annotate/src/app.cpp
@@ -2,7 +2,24 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-#include <vector>
+#include "llvm/AsmParser/Parser.h"
+#include "llvm/Bitcode/ReaderWriter.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "pass.hpp"
+
+
+using namespace std;
+using namespace llvm;
+using llvm::legacy::PassManager;
 
 namespace autopledge {
 
@@ -14,8 +31,34 @@ namespace autopledge {
 
 }
 
+namespace {
+    cl::opt<string>
+            inPath{cl::Positional,
+                   cl::desc("<Module to analyze>"),
+                   cl::value_desc("bitcode filename"), cl::Required};
+}
+
 int main(int argc, const char **argv) {
     llvm::cl::ParseCommandLineOptions(argc, argv);
 
-    std::cout << "test!\n";
+    sys::PrintStackTraceOnErrorSignal();
+    llvm::PrettyStackTraceProgram X{argc, argv};
+    llvm_shutdown_obj shutdown;
+    cl::ParseCommandLineOptions(argc, argv);
+
+    // Construct an IR file from the filename passed on the command line.
+    LLVMContext &context = getGlobalContext();
+    SMDiagnostic err;
+    unique_ptr<Module> module = llvm::parseIRFile(inPath.getValue(), err, context);
+
+    if (!module.get()) {
+        errs() << "Error reading bitcode file.\n";
+        err.print(argv[0], errs());
+        return -1;
+    }
+
+    // Build up all of the passes that we want to run on the module.
+    PassManager pm;
+//    pm.add(new callgraphs::CallGraphPass);
+    pm.run(*module);
 }
